bbb92db5ad7ee6ab869a0f7c1bb42af14c7f9bb6
Kosmonaft36 <Kosmonaft2007@ya.ru>
2022-08-09 17:04:04 +0300

    внесена задача 14

---
+++ Example_14/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+
+Console.Clear();
+// int number = new Random().Next(1, 10000);
+int number = 9;                              //    для ручной проверки
+int a = 7;
+int b = 23;
+string txtResult = " ";
+
+
+Console.WriteLine("Hello, World!");
