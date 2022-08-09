11a9862031286bfcbd0bc3c39e00424ed5829c5f
Kosmonaft36 <Kosmonaft2007@ya.ru>
2022-08-09 16:32:27 +0300

    внесена задача 9

--- Ex_09/Program.cs
+++ Ex_09/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введиte");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number > 99 && Number < 1000)
+{
+int a = Number%10;
+
+int b = Number/100;
+
+int i = (Number - (Number/100)*100) / 10;
+
+Console.WriteLine("Первое число: " + b);
+Console.WriteLine("Второе число: " + i);
+Console.WriteLine("Третье число: " + a);
+}
+if (Number > 0 && Number <= 99 )
+{
+    Console.WriteLine("Третьего числа нема ");
+}
+else
+    Console.WriteLine("Не верны данные ");
+
