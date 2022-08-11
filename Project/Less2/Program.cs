136e987555907b9e2f568cf3ad2477203df72b1c
Kosmonaft36 <Kosmonaft2007@ya.ru>
2022-08-11 17:38:09 +0300

    com доработать

--- Project/Less2/Program.cs
+++ Project/Less2/Program.cs
@@ -22,6 +22,47 @@ Sample Output:
 
 Совпадение 2 элементов
 */
-Console.Clear();
-Console.Write("Введи  число больше 4 символов: ");
+// Console.Clear();
+// Console.WriteLine("Введи первые числа: ");
 
+namespace ConsoleApplication3
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // string[] MyArray_1 = new string[] { "один", "два", "три", "четыре" };
+            // string[] MyArray_2 = new string[] {"один","один", "один", "один", "два", "два", "два", "три", "три"};
+
+
+            Console.Write("Введи первые 5 чисел : ");
+            
+            string nameSec = Console.ReadLine();
+            string[] MyArray_1 = nameSec.Split(' ');
+
+            Console.Write("Введи вторые 5 чисел : ");
+           
+            string nameSec2 = Console.ReadLine();
+            string[] MyArray_2 = nameSec2.Split(' ');
+            int Count = 0;
+
+            for (int i = 0; i < MyArray_1.Length; i++)
+            {
+                for (int j = 0; j < MyArray_2.Length; j++)
+                {
+                    if (MyArray_1[i] == MyArray_2[j])
+                        Count++;
+                }
+                Console.WriteLine("Элемент массива MyArray_1: '{0}' повторяются в массиве MyArray_2 {1} раз", MyArray_1[i], Count);
+
+                Console.WriteLine("Совпадение {1} элементов", MyArray_1[i], Count);
+                Count = 0;
+            }
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
