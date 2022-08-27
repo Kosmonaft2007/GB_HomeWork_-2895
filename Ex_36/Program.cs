45bff43d37b7e20ea4c6d71772f712f3e3bcb42c
Vadim <kosmonaft2007@ya.ru>
2022-08-27 11:25:18 +0300

    Ex23

---
+++ Ex_36/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+*/
+
+Console.Clear();
+
+
+int size = 5;
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int sum = 0;
+    System.Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+        if (array[i] % 2 == 0) sum += array[i];
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Сумма чисел, стоящих на четной позиции, соcтаваляет {sum} ");
+    System.Console.WriteLine();
+}
+
+PrintArray(FillArray(size));
