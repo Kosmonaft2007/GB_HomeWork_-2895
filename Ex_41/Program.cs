15f2d3adcc6e047215b456545b0351d4f2c52d95
Vadim <kosmonaft2007@ya.ru>
2022-08-28 07:20:24 +0300

    задача 41

---
+++ Ex_41/Program.cs
@@ -0,0 +1,60 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-10, 10);
+    }
+    return array;
+}
+
+void MMArray(int[] array)
+{
+    int max = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) max += 1;
+
+    }
+    System.Console.WriteLine($"Сумма  {max} ");
+}
+
+void PrintArray(int[] array)
+{
+    int sum = 0;
+    int index = 0;
+
+
+    System.Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+
+        if (array[i] > 0) index = i;
+
+        sum += index;
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Количество положительных элементов массива {sum} ");
+    System.Console.WriteLine();
+
+}
+
+void Main()
+{
+    int size = 10;
+
+    Console.Clear();
+
+    Console.WriteLine($"\nЗадача 41.  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.\n");
+
+
+    // PrintArray(FillArray(size));
+    MMArray(FillArray(size));
+
+}
+
+Main();
---
+++ Ex_46/Program.cs
@@ -0,0 +1,95 @@
+﻿// Без методов черновой 
+/*
+int m = 3;
+int n = 5;
+int[,] matrix = new int[m,n];
+
+// new Random().Next(1,  10); 
+for (int row = 0; row < m; row++) //  по строчкам 
+{
+    for (int column = 0; column < n; column++) //  по колонкам  
+    {
+        matrix[row, column] = new Random().Next(1,  10); 
+        System.Console.Write($"{matrix[row, column],3}");
+    }
+    System.Console.WriteLine();
+}
+*/
+
+// с методами 
+/*
+1 - создание массива 
+2 - заполнение массива 
+3 - печать 
+4 -
+
+*/
+int [,] CreateArray(int rows, int сolumns) // метот который принимает столбцы и строки 
+{
+    return new int [rows,  сolumns];
+}
+
+void FillArray(int[,] array, int minValue, int maxValue)
+{
+   
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int row = 0; row < rows; row++) //  по строчкам 
+    {
+        for (int column = 0; column < columns; column++) //  по колонкам  
+        {
+            array[row, column] = new Random().Next(minValue,  maxValue); 
+        }
+        
+    }
+}
+
+void PrintArray (int [,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int row = 0; row < rows; row++) //  по строчкам 
+    {
+        for (int column = 0; column < columns; column++) //  по колонкам  
+        {
+            Console.Write($"{array[row, column],3}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Input(string text)
+{
+    
+    // int Parse(); //  преобразовать строку в число 
+    // int.TryParse(); // я попробую преобразовать, если получится лили не получится я сообщу 
+    bool f = false;
+    int number = 0;
+    while( !f ) //цикл пока не фолс
+    {
+        System.Console.WriteLine(text);
+        string str = Console.ReadLine();
+        f = int.TryParse(str, out number);
+    }
+    return number;
+}
+
+void Main ()
+{
+     int m = Input("rows");
+    int n = Input("columns");
+    // int[,] matrix = CreateArray(m,n);
+
+if (m>= 0 && n >=0)
+{int [,] matrix = CreateArray( n,m);
+FillArray(matrix, 10, 20);
+PrintArray(matrix);
+
+Console.WriteLine("Hello, World!");
+}
+
+}
+
+Main();
